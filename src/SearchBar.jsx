b51439042b61413371edab2bfc31454861ac98c5
unknown <k.rafati@outlook.com>
2023-08-07 15:25:23 +0330

    completed search bar component

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -4,8 +4,41 @@ import { ReactComponent as SearchIcon } from './assets/search.svg';
 const SearchBarComponent = () => {
     let placeHoldertxt = 'Search'
 
+    const SearchBarStyle = {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, 5vw)',
+        gap: '0',
+        gridAutoRows: '5vh',
+    }
+ 
+    const inputStyle = {
+        display: 'grid',
+        gridAutoRows: '5vh',
+        gridColumn: '1/span 5',
+        borderTopLeftRadius: '10px',
+        borderBottomLeftRadius: '10px',
+    }
+    
+    const btnStyle = {
+        borderRadius: '0',
+        backgroundColor: '#fff',
+        gridColumn: '6/span 1',
+        borderTopRightRadius: '10px',
+        borderBottomRightRadius: '10px',
+    }
+    
+    const iconStyle = {
+        width: '100%',
+        height: '100%',
+    }
+
     return (
-        <input typeof="text" id="searchBar" placeholder={placeHoldertxt}></input>
+        <div id="searchBar"  style={SearchBarStyle}>
+            <input typeof="text" placeholder={placeHoldertxt} style={inputStyle}></input>
+            <button style={btnStyle}>
+                <SearchIcon style={iconStyle} />
+            </button>
+        </div>
     )
 }
 
